4259238a7d836d5c39c63f5599a0f0930e57d085
Frank Purcell <fxpurcell@gmail.com>
2021-07-26 14:18:58 -0700

    fix(python junk): initial add

---
+++ ott/log_parser/control/parser.py
@@ -0,0 +1,42 @@
+import datetime
+import logging
+import os
+from pathlib import Path
+from boltons.tbutils import ParsedException
+from parse import parse, with_pattern
+
+
+LOGGING_DEFAULT_DATEFMT = f"{logging.Formatter.default_time_format},%f"
+
+
+# TODO better pattern
+@with_pattern(r"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d,\d\d\d")
+def parse_logging_time(raw):
+    return datetime.datetime.strptime(raw, LOGGING_DEFAULT_DATEFMT)
+
+
+def from_log(file: os.PathLike, fmt: str):
+    chunk = ""
+    custom_parsers = {"asctime": parse_logging_time}
+
+    with Path(file).open() as fp:
+        for line in fp:
+            parsed = parse(fmt, line, custom_parsers)
+            if parsed is not None:
+                yield parsed
+            else:  # try parsing the stacktrace
+                chunk += line
+                try:
+                    yield ParsedException.from_string(chunk)
+                    chunk = ""
+                except (IndexError, ValueError):
+                    pass
+
+
+if __name__ == "__main__":
+    for parsed_record in from_log(
+        file="so.log",
+        fmt="{asctime:asctime} - {module} - {levelname} - {message}"
+    ):
+        print(parsed_record)
+
