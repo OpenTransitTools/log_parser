e6a5343a8d57b38e43d309452caa1a650f34e666
Frank Purcell <fxpurcell@gmail.com>
2021-07-26 13:40:49 -0700

    fix(logs): initial add

---
+++ README.md
@@ -0,0 +1,4 @@
+log_parser
+====
+
+parse apache logs, pulling out OTP trip planner urls, and parsing that data into a .csv or .json file
---
+++ setup.py
@@ -0,0 +1,49 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'sqlalchemy',
+    'zope.sqlalchemy',
+    'psycopg2-binary',
+]
+
+extras_require = dict(
+    dev=[],
+)
+
+
+setup(
+    name='ott.log_parser',
+    version='0.1.0',
+    description='Open Transit Tools - parse server log files / OTP trip plan urls',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    author="Open Transit Tools",
+    author_email="info@opentransittools.org",
+    dependency_links=[
+        'git+https://github.com/OpenTransitTools/gtfsdb.git#egg=gtfsdb-1.0.0',
+        'git+https://github.com/OpenTransitTools/utils.git#egg=ott.utils-0.1.0',
+    ],
+    license="Mozilla-derived (http://opentransittools.com)",
+    url='http://opentransittools.com',
+    keywords='ott, osm, otp, gtfs, vector, tiles, maps, transit',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    extras_require=extras_require,
+    tests_require=requires,
+    test_suite="ott.log_parser.tests",
+    entry_points="""
+        [console_scripts]
+        parse = ott.log_parser.control.parser:main
+    """,
+)
