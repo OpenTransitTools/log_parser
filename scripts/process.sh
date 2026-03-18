be7d208992ab39840e3659c30687132fbfc5c42b
Frank Purcell <fxpurcell@gmail.com>
2026-03-17 17:11:43 -0700

    fix(process.sh): clean up the process script

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,6 +1,6 @@
 # 1 1 * * * source ~/.bashrc; cd ~/log_parser; flock -n /tmp/loader.lock -c '~/process.sh >> process.out 2>&1'
 
-OUT_DIR=processing
+OUT_DIR=$HOME/processing
 
 # copy yesterday's log file to the processing dir
 scripts/cp_logs.sh
@@ -8,7 +8,7 @@ DT=`date -d "1 day ago" '+%Y-%m-%d'`
 
 # clear the db, load db and generate .csv data
 poetry run loader -c -l CLEAR
-poetry run load_and_post_process -c -l ~/$OUT_DIR/
+poetry run load_and_post_process -c -l $OUT_DIR
 poetry run publisher
 poetry run stats > stats.txt
 
@@ -17,6 +17,5 @@ mv ./trip_requests.csv ~/var/otp_trips/${DT}_trips.csv
 mv ./stats.txt ~/var/otp_trips_transferred/${DT}_stats.txt
 
 # backup log dir to tmp
-# note: rj-pd-mapetl server's /tmp is too small to copy
-#rm -rf /tmp/$OUT_DIR
-#mv ~/$OUT_DIR /tmp
+rm -rf $OUT_DIR-bkup
+mv $OUT_DIR $OUT_DIR-bkup
