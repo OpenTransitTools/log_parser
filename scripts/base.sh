a66f09f429e7613b501f96e73140fb9922a39fb7
Frank Purcell <fxpurcell@gmail.com>
2026-03-26 06:55:13 -0700

    fix(process): copy all log files >5mb in size to processing folder (pick up straggler 01 servers)

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,6 +1,6 @@
 DAYS=${1:-1}
 ADAYS=$((${DAYS} - 1))
-SIZE=${2:-25}
+SIZE=${2:-5}
 LOG_DIR=${3:-$HOME/var/httpd_logs}
 OUT_DIR=${4:-$HOME/processing}
 SVR_DIRS=${5:-maps9 maps10 cs-pd-pubweb01.tri-met.org rj-pd-pubweb01.tri-met.org cs-pd-pubweb03.tri-met.org rj-pd-pubweb03.tri-met.org}
