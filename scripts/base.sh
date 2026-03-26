db9a0c1a7e7c45c3c7d4fd821243767fb6e95602
Frank Purcell <fxpurcell@gmail.com>
2026-03-25 18:28:41 -0700

    fix(process): add the new pubweb03 servers to the process

--- scripts/base.sh
+++ scripts/base.sh
@@ -3,4 +3,4 @@ ADAYS=$((${DAYS} - 1))
 SIZE=${2:-25}
 LOG_DIR=${3:-$HOME/var/httpd_logs}
 OUT_DIR=${4:-$HOME/processing}
-SVR_DIRS=${5:-maps8 maps9 maps10 cs-pd-pubweb01.tri-met.org rj-pd-pubweb01.tri-met.org}
+SVR_DIRS=${5:-maps9 maps10 cs-pd-pubweb01.tri-met.org rj-pd-pubweb01.tri-met.org cs-pd-pubweb03.tri-met.org rj-pd-pubweb03.tri-met.org}
