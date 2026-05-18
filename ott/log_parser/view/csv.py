30429598000f8364977cd705df03ade5f67c3c3c
Frank Purcell <fxpurcell@gmail.com>
2026-05-18 10:20:45 -0700

    feat(agencies): util to count the various agencies in the csv that goes to UL

--- ott/log_parser/view/csv.py
+++ ott/log_parser/view/csv.py
@@ -1,6 +1,23 @@
+from collections import Counter
+from ott.utils.parse.cmdline.base_cmdline import file_cmdline
+from ott.utils import file_utils
+
 import logging
 log = logging.getLogger(__file__)
 
 
-def csv(data: dict, file_name: str):
-    return None
+def modes_plus_agencies(prog_name='poetry run view_csv', file_name='trip_requests.csv'):
+    cmdline = file_cmdline(prog_name, file_name)
+    print(f"{cmdline.file}")
+
+    data = []
+    for r in file_utils.read_csv(cmdline.file):
+        companies = r.get('agencies').strip()
+        if len(companies) > 1:
+            sep = " -> "
+        data.append(f"{companies}{sep}{r.get('modes')}")
+    counts = Counter(data)
+    for s in sorted(counts.items()):
+        print(f"{s[1]:8} {s[0]}")
+
+    return 0
