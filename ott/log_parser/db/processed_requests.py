72236e6e5c02ccf8b965f05466bb0c5c32fb4117
Frank Purcell <fxpurcell@gmail.com>
2026-06-10 09:29:53 -0700

    fix(extensions): check for Park & Ride in geocode too

--- ott/log_parser/db/processed_requests.py
+++ ott/log_parser/db/processed_requests.py
@@ -288,7 +288,7 @@ class ProcessedRequests(Base):
         if len(fm) > 0 or len(to) > 0:
             self.ft_gps = utils.parse_ft_metadata(fm, to, "GPS")
             self.ft_stop = utils.parse_ft_metadata(fm, to, "STOP")
-            self.ft_pr = utils.parse_ft_metadata(fm, to, "PR")
+            self.ft_pr = utils.parse_ft_metadata(fm, to, "PR", self.ft_pr)
             self.ft_map = utils.parse_ft_metadata(fm, to, "MAP")
 
     def parse_extensions(self, qs):
